a1a45de2a58903a8f8311a108f78a783482dbce1
spaul-12 <sp.21u10434@btech.nitdgp.ac.in>
2022-11-28 00:43:59 +0530

    fixed cam window size and added colours

--- game.py
+++ game.py
@@ -6,8 +6,11 @@ import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+#cap.set(3, 1280)
+#cap.set(4, 720)
+clr_r=255
+clr_g=0
+clr_b=0
 
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
@@ -19,8 +22,11 @@ class SnakeGameClass:
         self.currentLength = 0  # total length of the snake
         self.allowedLength = 150  # total allowed Length
         self.previousHead = 0, 0  # previous head point
-
-        self.imgFood = cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
+        self.clr_r=255
+        self.clr_b=0
+        self.clr_g=0
+        self.imgFood =cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
+        #cv2.imshow("food",self.imgFood)
         self.hFood, self.wFood, _ = self.imgFood.shape
         self.foodPoint = 0, 0
         self.randomFoodLocation()
@@ -29,9 +35,9 @@ class SnakeGameClass:
         self.gameOver = False
 
     def randomFoodLocation(self):
-        self.foodPoint = random.randint(100, 500), random.randint(100, 300)
+        self.foodPoint = random.randint(100, 1000), random.randint(100, 600)
 
-    def update(self, imgMain, currentHead):
+    def update(self, imgMain, currentHead,clr_r,clr_b,clr_g):
 
         if self.gameOver:
             cvzone.putTextRect(imgMain, "Game Over", [300, 400],
@@ -41,13 +47,12 @@ class SnakeGameClass:
         else:
             px, py = self.previousHead
             cx, cy = currentHead
-
+            
             self.points.append([cx, cy])
             distance = math.hypot(cx - px, cy - py)
             self.lengths.append(distance)
             self.currentLength += distance
             self.previousHead = cx, cy
-
             # Length Reduction
             if self.currentLength > self.allowedLength:
                 for i, length in enumerate(self.lengths):
@@ -59,23 +64,30 @@ class SnakeGameClass:
 
             # Check if snake ate the Food
             rx, ry = self.foodPoint
-            if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and ry - self.hFood // 2 < cy < ry + self.hFood // 2:
+            if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and \
+                    ry - self.hFood // 2 < cy < ry + self.hFood // 2:
                 self.randomFoodLocation()
                 self.allowedLength += 50
                 self.score += 1
                 print(self.score)
+                clr_r=random.randint(20,255)
+                clr_b=random.randint(20,255)
+                clr_g=random.randint(20,255)
 
             # Draw Snake
             if self.points:
                 for i, point in enumerate(self.points):
                     if i != 0:
-                        cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+                        
+                        cv2.line(imgMain, self.points[i - 1], self.points[i], (clr_g,clr_b,clr_r), 20)
                 cv2.circle(imgMain, self.points[-1], 20, (0, 255, 0), cv2.FILLED)
 
             # Draw Food
-            imgMain = cvzone.overlayPNG(imgMain, self.imgFood, (rx - self.wFood // 2, ry - self.hFood // 2))
+            imgMain = cvzone.overlayPNG(imgMain, self.imgFood,
+                                        (rx - self.wFood // 2, ry - self.hFood // 2))
 
-            cvzone.putTextRect(imgMain, f'Score: {self.score}', [50, 80], scale=3, thickness=3, offset=10)
+            cvzone.putTextRect(imgMain, f'Score: {self.score}', [50, 80],
+                               scale=3, thickness=3, offset=10)
 
             # Check for Collision
             pts = np.array(self.points[:-2], np.int32)
@@ -83,7 +95,7 @@ class SnakeGameClass:
             cv2.polylines(imgMain, [pts], False, (0, 255, 0), 3)
             minDist = cv2.pointPolygonTest(pts, (cx, cy), True)
 
-            if -1 <= minDist <= 1:
+            if -1<= minDist <=1:
                 print("Hit")
                 self.gameOver = True
                 self.points = []  # all points of the snake
@@ -93,21 +105,33 @@ class SnakeGameClass:
                 self.previousHead = 0, 0  # previous head point
                 self.randomFoodLocation()
 
-        return imgMain
+        return (imgMain,clr_r,clr_b,clr_g)
 
 
-game = SnakeGameClass("Donut.png")
+game = SnakeGameClass("tux(2.1).png")
 
 while True:
     success, img = cap.read()
+    width=int(img.shape[1]*2)
+    height=int(img.shape[0]*2)
+    dimension=(width,height)
+    img=cv2.resize(img,dimension,interpolation=cv2.INTER_CUBIC)
     img = cv2.flip(img, 1)
     hands, img = detector.findHands(img, flipType=False)
 
     if hands:
+        #print("1")
         lmList = hands[0]['lmList']
         pointIndex = lmList[8][0:2]
-        img = game.update(img, pointIndex)
+        #print(clr_r)
+        img,clr_r,clr_b,clr_g = game.update(img, pointIndex,clr_r,clr_b,clr_g)
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord('r'):
         game.gameOver = False
+    elif key & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+#key == ord('r')
